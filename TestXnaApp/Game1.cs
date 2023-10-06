5aefbf45156adb7aac3c84f3e2690f452efd4113
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2023-10-06 03:06:33 +0200

    Added nuget for FNA 23.10 and a TestXnaApp sample

---
+++ TestXnaApp/Game1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TestXnaApp;
+
+public class Game1 : Microsoft.Xna.Framework.Game
+{
+	public Game1()
+	{
+		graphics = new GraphicsDeviceManager(this);
+		Content.RootDirectory = "Content";
+	}
+
+	GraphicsDeviceManager graphics;
+	SpriteBatch? spriteBatch;
+
+	protected override void LoadContent()
+	{
+		// Create a new SpriteBatch, which can be used to draw textures.
+		spriteBatch = new SpriteBatch(GraphicsDevice);
+		// TODO: use this.Content to load your game content here
+	}
+
+	protected override void UnloadContent()
+	{
+		// TODO: Unload any non ContentManager content here
+	}
+
+	protected override void Update(GameTime gameTime)
+	{
+		// Allows the game to exit
+		if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+			this.Exit();
+		// TODO: Add your update logic here
+		base.Update(gameTime);
+	}
+
+	protected override void Draw(GameTime gameTime)
+	{
+		GraphicsDevice.Clear(Color.CornflowerBlue);
+		// TODO: Add your drawing code here
+		base.Draw(gameTime);
+	}
+}
---
+++ TestXnaApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+namespace TestXnaApp;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		using (Game1 game = new Game1())
+		{
+			game.Run();
+		}
+	}
+}
